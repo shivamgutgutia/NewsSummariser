31eaf10b598cd776f18ba44cbc4a3fc69152008c
Shivam Gutgutia <gutgutia159@gmail.com>
2024-11-06 19:58:02 +0530

    Minor changes

--- modeltraining.py
+++ modeltraining.py
@@ -1,21 +1,15 @@
-# %%
-print("This might take a while to run",end="\n\n")
+print("This might take a while to run", end="\n\n")
 
 print("IMPORTING LIBRARIES", end="\n")
+
 import time
 import pandas as pd
-
-
 import torch
 from torch import cuda
 from torch.utils.data import Dataset, DataLoader
-
-
 from transformers import BartTokenizer, BartForConditionalGeneration
 import os
 import shutil
-
-# %%
 
 device = "cuda" if cuda.is_available() else "cpu"
 print(f"Using device: {device}", end="\n\n")
@@ -26,41 +20,27 @@ if os.path.exists(folder_path):
     shutil.rmtree(folder_path)
 os.makedirs(folder_path)
 
-# %%
-
 torch.backends.cudnn.deterministic = True
-
 
 new_repo = "text_summarizer"
 repo_name = "EducativeCS2023/bart-base-summarization"
 
-# %%
-
 print("RETRIEVING TRAINING DATA", end="\n\n")
 
 df = pd.read_csv("BBCarticles.csv", encoding="latin-1")
-
-
 df = df[["Text", "Summary"]]
 df.Text = "summarize: " + df.Text
-
 split_ratio = 0.025
-
 train_dataset = df.sample(frac=split_ratio).reset_index(drop=True)
-
 eval_dataset = (
     df.drop(train_dataset.index).sample(frac=split_ratio).reset_index(drop=True)
 )
 
-
 print("Training Dataset Size:", train_dataset.shape, end="\n\n")
 print("Evaluation Dataset Size:", eval_dataset.shape, end="\n\n")
+print(df.head(3), end="\n\n")
 
 
-print(df.head(3),end="\n\n")
-
-
-# %%
 class CustomDataset(Dataset):
     def __init__(self, dataframe, tokenizer, source_len, summ_len):
         self.tokenizer = tokenizer
@@ -105,31 +85,19 @@ class CustomDataset(Dataset):
         }
 
 
-# %%
-
-
 tokenizer = BartTokenizer.from_pretrained(repo_name)
-
-
 training_set = CustomDataset(train_dataset, tokenizer, 512, 150)
 eval_set = CustomDataset(eval_dataset, tokenizer, 512, 150)
-
-
 training_loader = DataLoader(training_set, batch_size=2, shuffle=True, num_workers=0)
 eval_loader = DataLoader(eval_set, batch_size=2, shuffle=False, num_workers=0)
 
-# %%
-
 print("INITIALISING BASE MODEL", end="\n\n")
+
 model = BartForConditionalGeneration.from_pretrained(repo_name)
-
-
 model = model.to(device)
-
 optimizer = torch.optim.Adam(params=model.parameters(), lr=1e-4)
 
 
-# %%
 def train(epoch, tokenizer, model, device, loader, optimizer):
     model.train()
     for batch_index, data in enumerate(loader, 0):
@@ -152,20 +120,16 @@ def train(epoch, tokenizer, model, device, loader, optimizer):
         optimizer.step()
 
 
-# %%
-
 print("TRAINING MODEL", end="\n\n")
 
 for epoch in range(2):
     print(f"Training epoch: {epoch+1}/{2}")
     train(epoch, tokenizer, model, device, training_loader, optimizer)
 
-# %%
 model.save_pretrained("./model")
 tokenizer.save_pretrained("./model")
 
 
-# %%
 def predict(tokenizer, model, device, loader):
     model.eval()
     predictions = []
@@ -206,43 +170,26 @@ def predict(tokenizer, model, device, loader):
     return predictions, actuals
 
 
-# %%
-
 print("PREDICTING OUTPUTS", end="\n\n")
 
 start_time = time.time()
-
 model = BartForConditionalGeneration.from_pretrained("./model")
 tokenizer = BartTokenizer.from_pretrained("./model")
-
-
 predictions, actuals = predict(tokenizer, model, device, eval_loader)
-
 results = pd.DataFrame({"predictions": predictions, "actuals": actuals})
-
 results.to_csv("results.csv")
-
 end_time = time.time()
 time_taken = end_time - start_time
 print(f"Time taken for predictions: {time_taken:.2f} seconds")
-
-results.head()
-
-# %%
+print(results.head())
 
 print("EVALUATING MODEL", end="\n\n")
+
 import evaluate
 
 rouge_score = evaluate.load("rouge")
-
-
 scores = rouge_score.compute(
     predictions=results["predictions"], references=results["actuals"]
 )
-
-
 rouge_scores_df = pd.DataFrame([scores]).transpose()
-
 print(rouge_scores_df.head())
-
-# %%
